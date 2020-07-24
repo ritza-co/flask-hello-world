3e96befbd021d94b8dfc67086ef024148d8e0b7c
Leandri Nieuwoudt <lean331@gmail.com>
2020-07-24 14:36:14 +0000

    add personalised url

--- mydemoapp.py
+++ mydemoapp.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello! This is a basic Flask app. "
 
+@app.route("/personalised/<name>")
+def personalised(name):
+    return f"Hello {name}"
+
+
 if __name__ == "__main__":
     app.run("0.0.0.0")
