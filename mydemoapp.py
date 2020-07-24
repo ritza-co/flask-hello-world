95ca710de8819410ada76122570a9608a0057bb9
Leandri Nieuwoudt <52743457+Lean3Viljoen94@users.noreply.github.com>
2020-07-24 14:33:57 +0200

    Create mydemoapp.py

---
+++ mydemoapp.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello! This is a basic Flask app. "
+
+if __name__ == "__main__":
+    app.run("0.0.0.0")
