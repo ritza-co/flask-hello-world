a9938d45d60610c4eda74accb7153fe721d48229
apricotvillage <apricotvillage@163.com>
2022-02-08 09:49:07 +0000

    update file

--- mydemoapp.py
+++ mydemoapp.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello! This is a basic Flask app. "
 
+@app.route("/<name>")
+def hello_name(name):
+    return f"Hello {name}"
+    
 if __name__ == "__main__":
     app.run("0.0.0.0")
